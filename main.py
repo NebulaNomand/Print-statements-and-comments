fca281faa230baecda842165c7be1d42a03515eb
NebulaNomand <osivayaonimi@gmail.com>
2024-12-18 17:54:28 +0000

    commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print ("Hello World!")
+
+# Printing my name
+print ("Hello My name is Onimi")
+
+# Printing fun fact about myself
+print ("I love playing football")
+
+# Performing and printing a simple calculation
+print ("The result of 20019  +  35708 is:",20019 + 35708)
